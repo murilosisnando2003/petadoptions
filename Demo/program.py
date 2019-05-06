0d08b6f4606e3e3175085f41647cd11dbf40ff54
Murilo Rodrigues <murilosisnando2003@hotmail.com>
2019-05-05 22:08:37 -0300

    go

---
+++ Demo/program.py
@@ -0,0 +1,9 @@
+def add(a,b):
+    return a + b
+
+input1 = input("Digita um numero: ")
+input2 = input("Digite um segundo numero: ")
+
+sum = add(input1,input2)
+print(f"{input1} + {input2} = {sum}")
+
