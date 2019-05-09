1131adbeecbabe8e722bb0267303dde33bc0d5e4
Murilo Rodrigues <murilosisnando2003@hotmail.com>
2019-05-08 22:46:49 -0300

    Final

--- wisdopets/wisdopets/urls.py
+++ wisdopets/wisdopets/urls.py
@@ -2,7 +2,10 @@
 
 from django.contrib import admin
 from django.urls import path
+from adoptions import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
+    path('adoptions/<int:id>/', views.pet_detail, name='pet_detail'),
 ]
